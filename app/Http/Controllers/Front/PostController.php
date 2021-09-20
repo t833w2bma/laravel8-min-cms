18920cde44d64691c89c18967b3844d2c2afbad6
root <root@localhost.localdomain>
2021-09-20 17:33:14 +0900

    renew master

--- app/Http/Controllers/Front/PostController.php
+++ app/Http/Controllers/Front/PostController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Front;
 use App\Http\Controllers\Controller;
 use App\Models\Post;
 use Illuminate\Http\Request;
+use App\Models\Tag;
 
 class PostController extends Controller
 {
@@ -13,16 +14,25 @@ class PostController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index() {
         //// 公開・新しい順に表示
         // $posts = Post::where('is_public', true)
         //             ->orderBy('published_at', 'desc')
         //             ->paginate(10);
         /* モデルのスコープ関数 */
-        $posts = Post::publicList(); // ← PostModelのscopePublicListメソッド実行｡上の部分が書いてある
 
-        return view('front.posts.index', compact('posts'));
+
+
+    public function index(string $tagSlug = null)
+    {
+        // 公開・新しい順に表示
+        $posts = Post::publicList($tagSlug);
+        $tags = Tag::all();
+
+        return view('front.posts.index', compact('posts', 'tags'));
     }
+
+
+
 
     /**
      * 詳細画面
@@ -41,60 +51,4 @@ class PostController extends Controller
 
 
 
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Post  $post
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Post $post)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Post  $post
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Post $post)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Post  $post
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Post $post)
-    {
-        //
-    }
 }
