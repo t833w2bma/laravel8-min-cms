36ebec67c4e04181eb68ca40fa7f1c12458c7053
satojiro13 <satojiro13@gmail.com>
2021-09-20 11:00:43 +0900

    saikai

--- app/Http/Controllers/Back/PostController.php
+++ app/Http/Controllers/Back/PostController.php
@@ -4,9 +4,13 @@ namespace App\Http\Controllers\Back;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Post;
+use App\Http\Requests\PostRequest;
+
 
 class PostController extends Controller
 {
+    
     public function index(){
         // 一覧画面
         $posts = Post::latest('id')->paginate(20);
@@ -14,69 +18,65 @@ class PostController extends Controller
 
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+ 
+
+
+    public function store(PostRequest $request){
+        $post = Post::create($request->all());
+        
+        if ($post) {
+            return redirect()
+                ->route('back.posts.edit', $post)
+                ->withSuccess('データを登録しました。');
+        } else {
+            return redirect()
+                ->route('back.posts.create')
+                ->withError('データの登録に失敗しました。');
+        }
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
+
+
+    public function create(){
+        return view('back.posts.create');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
+
+
+
+    public function edit( Post $post ) {
+        
+        return view('back.posts.edit', compact('post'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
+
+
+
+    public function update(PostRequest $request, Post $post) {
+        if ($post->update($request->all())) {
+            $flash = ['success' => 'データを更新しました。'];
+        } else {
+            $flash = ['error' => 'データの更新に失敗しました'];
+        }
+     
+        return redirect()
+            ->route('back.posts.edit', $post)
+            ->with($flash);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
+
+
+    public function destroy(Post $post)
     {
-        //
+        if ($post->delete()) {
+            $flash = ['success' => 'データを削除しました。'];
+        } else {
+            $flash = ['error' => 'データの削除に失敗しました'];
+        }
+     
+        return redirect()
+            ->route('back.posts.index')
+            ->with($flash);
     }
 }
---
+++ app/Http/Requests/PostRequest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class PostRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize(){
+        // return false; 
+        return true;  // ← 認証されている場合許可
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules(){
+        return [
+            'title' => 'required|max:22',
+            'body' => 'max:1000',
+            'is_public' => 'required|numeric',
+            'published_at' => 'required|date_format:Y-m-d H:i',
+        ];
+    }
+
+
+    public function attributes(){
+        return [
+            'title' => 'タイトル',
+            'body' => '内容',
+            'is_public' => 'ステータス',
+            'published_at' => '公開日',
+        ];
+    }
+
+
+}
